7f282defd4be7e4082f80e8dc2d5589f4b29742d
yasmimsoaresverissimo <yasmim.soares.verissimo@gmail.com>
2023-09-20 20:52:03 -0300

    Atualização de formulario

--- src/component/Documento/Documento.tsx
+++ src/component/Documento/Documento.tsx
@@ -2,12 +2,31 @@ import React from "react";
 import Form from "../../compenentes-compartilhados/Form/Form";
 import Input from "../../compenentes-compartilhados/Input/Input";
 import Conteudo from "../../compenentes-compartilhados/Conteudo/Conteudo";
+import { Grid } from "@mui/material";
 
 function Documento() {
 
     return <Conteudo >
         <Form titulo="Criar documento">
-            <Input label="Responsável pela assinatura"/>
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                <Input label="Responsável pela assinatura"/>
+                </Grid>
+                <Grid item xs={6}>
+                <Input label="nome"/>
+                </Grid>
+            </Grid>
+            
+            <Grid container spacing={2}>
+                <Grid item xs={5}>
+                <Input label="idade"/>
+                </Grid>
+                <Grid item xs={4}>
+                <Input label="cpf"/>
+                </Grid>
+            </Grid>
+
+
         </Form>
     </Conteudo>
 
